27d074d25ea585bbce6372da147f3cdde004a5ba
42null <averyparanoiduser@gmail.com>
2022-10-28 18:09:18 -0500

    Video window link with thumbnail now working with more locations

--- youtubewithactualcorners.js
+++ youtubewithactualcorners.js
@@ -13,12 +13,11 @@ style.innerHTML = `
         border-radius: 0px 2px 2px 0px !important;
     }
 /*VIDEO WINDOW LINK INSIDE VIDEO*/
-    .ytp-ce-element.ytp-ce-video.ytp-ce-element-show.ytp-ce-medium-round.ytp-ce-top-left-quad.ytp-ce-size-640{
+    .ytp-ce-element.ytp-ce-video.ytp-ce-element-show.ytp-ce-size-640{
         border-radius: 0px !important;
     }
     `;
 /* Still Need to polish
-
 */
 
 document.head.appendChild(style);
