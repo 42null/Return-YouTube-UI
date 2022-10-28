e714eb0201e872f5dcc6e2b18a713b720f7b4006
42null <averyparanoiduser@gmail.com>
2022-10-28 16:12:04 -0500

    Thumbnails and search box look like they orignaly did

--- youtubewithactualcorners.js
+++ youtubewithactualcorners.js
@@ -1,31 +1,16 @@
 const style = document.createElement('style');
 style.innerHTML = `
-#ttttttt{
-    border-radius: 0 !important;
-}
-
-#ttttttvoice-search-button{
-    background-color: red !important;
-    border-radius: 100% !important;
-}
-#search{
-    border-radius: 1px !important;
-}
-#ttttttavatar-btn > img{
-    background-color: red !important;
-    border-radius: 100vh !important;
-}
-#thumbnail{
-    background-color: red !important;
-    border-radius: 0 !important;
-}
+    #thumbnail{
+        border-radius: 0 !important;
+    }
+    #container.ytd-searchbox {
+        border-radius: 2px 0px 0px 2px  !important;
+    }
+    button#search-icon-legacy {
+        border-radius: 0px 2px 2px 0px !important;
+    }
     `;
-
-// style-scope ytd-topbar-menu-button-renderer no-transition
-
 /* Still Need to polish
-
-#search
 
 */
 
