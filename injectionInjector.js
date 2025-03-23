846576ddf0fe8af9849c41bfc3094a54a65207e1
42null <averyparanoiduser@gmail.com>
2025-03-23 11:48:23 -0500

    Lower right miniplayer fix

--- injectionInjector.js
+++ injectionInjector.js
@@ -125,14 +125,14 @@ function setInjectionStateHelper(state, id, filePath){
     }
     let element = document.getElementById(id);
 
-    if (state === true) {
+    if (state) {
         // TODO: Make more efficient, right now trys removal and re-add
         if(element === null) {
             element = createElementLink(filePath);
             element.id = id;
             document.head.appendChild(element);
         }
-    } else if (state === false) {
+    } else {
         try{
             element.parentElement.removeChild(element);
         }catch(e){}
@@ -157,64 +157,44 @@ function settingsToActions(){
             const key = keys[i];
             const value = applySettings[keys[i]].value;
 
-            // if(typeof value == "boolean"){
-            if(true){
-                switch(key) {
-                    case "UN_ROUNDED_SEARCH":
-                        setInjectionStateHelper(value, "injection_parts/primary/searchbox.css");
-                        break;
-                    case "SUBSCRIBE_BUTTON_COLOR":
-                        setInjectionStateHelper(value, "injection_parts/primary/subscribe_button_color.css");
-                        break;
-                    case "SUBSCRIBE_BUTTON_SHAPE":
-                        setInjectionStateHelper(value, "injection_parts/primary/subscribe_button_shape.css");
-                        break;
-                    case "UN_ROUNDED_MENUS":
-                        setInjectionStateHelper(value, "injection_parts/primary/unrounded_menus.css");
-                        break;
-                    case "UN_ROUNDED_MAIN_PLAYER":
-                        setInjectionStateHelper(value, "injection_parts/primary/unrounded_main_player.css");
-                        break;
-                    case "UN_ROUNDED_THUMBNAILS_AND_SECONDARY_PLAYERS":
-                        setInjectionStateHelper(value, "injection_parts/primary/unrounded_thumbnails_and_secondary_players.css");
-                        setInjectionStateHelper(value, "injection_parts/primary/unrounded_image_posts.css");//TODO: Move to own setting?
-                        break;
-                    case "BAR_BUTTONS":
-                        setInjectionStateHelper(value, "injection_parts/primary/otherFormattingFromViews.css");
-                        break;
-                    case "SAVE_BEFORE_SHARE":
-                        // if(value){
-                        //     setInjectionStateHelper(true, "injection_parts/primary/save_visible_before_clip.js");
-                        // }else{
-                        //     save_visible_before_clip(false);
-                        // }
-                        break;
-                }
-                // setInjectionStateHelper(value, "injection_parts/primary/text_formatting.css");
-            // }else{
-                if(key === "VIDEOS_PER_ROW"){
-                    setProperty("return-youtube-ui-videos-per-row", value);
-                    setInjectionStateHelper( "injection_parts/primary/homepage_videos_per_row.css");
-                }else if(key === "PERCENT_MORE_SPACE_TO_ACTIONS_BAR"){
-                    setProperty("return-youtube-ui-percent-more-actions-bar-space", value+"%");
-                    setInjectionStateHelper( "injection_parts/primary/percent_more_actions_bar.css");
-                }
-                // setInjectionStateHelper(value, "injection_parts/primary/text_formatting.css");
-            // }else{
-                if(key === "VIDEOS_PER_ROW"){
-                    setProperty("return-youtube-ui-videos-per-row", value);
-                    setInjectionStateHelper( "injection_parts/primary/homepage_videos_per_row.css");
-                }else if(key === "PERCENT_MORE_SPACE_TO_ACTIONS_BAR"){
-                    setProperty("return-youtube-ui-percent-more-actions-bar-space", value+"%");
-                    setInjectionStateHelper( "injection_parts/primary/percent_more_actions_bar.css");
-                }
-            }else{
-                if(key === "VIDEOS_PER_ROW"){
-                    var r = document.querySelector(':root');
-                    r.style.setProperty('--return-youtube-ui-videos-per-row', value);
-                    setInjectionStateHelper(false, "injection_parts/return/homepage_videos_per_row.css");
-                    setInjectionStateHelper(true, "injection_parts/return/homepage_videos_per_row.css");
-                }
+            switch(key) {
+                case "UN_ROUNDED_SEARCH":
+                    setInjectionStateHelper(value, "injection_parts/primary/searchbox.css");
+                    break;
+                case "SUBSCRIBE_BUTTON_COLOR":
+                    setInjectionStateHelper(value, "injection_parts/primary/subscribe_button_color.css");
+                    break;
+                case "SUBSCRIBE_BUTTON_SHAPE":
+                    setInjectionStateHelper(value, "injection_parts/primary/subscribe_button_shape.css");
+                    break;
+                case "UN_ROUNDED_MENUS":
+                    setInjectionStateHelper(value, "injection_parts/primary/unrounded_menus.css");
+                    break;
+                case "UN_ROUNDED_MAIN_PLAYER":
+                    setInjectionStateHelper(value, "injection_parts/primary/unrounded_main_player.css");
+                    break;
+                case "UN_ROUNDED_THUMBNAILS_AND_SECONDARY_PLAYERS":
+                    setInjectionStateHelper(value, "injection_parts/primary/unrounded_thumbnails_and_secondary_players.css");
+                    setInjectionStateHelper(value, "injection_parts/primary/unrounded_image_posts.css");//TODO: Move to own setting?
+                    break;
+                case "BAR_BUTTONS":
+                    setInjectionStateHelper(value, "injection_parts/primary/otherFormattingFromViews.css");
+                    break;
+                case "SAVE_BEFORE_SHARE":
+                    // if(value){
+                    //     setInjectionStateHelper(true, "injection_parts/primary/save_visible_before_clip.js");
+                    // }else{
+                    //     save_visible_before_clip(false);
+                    // }
+                    break;
+            }
+            // setInjectionStateHelper(value, "injection_parts/primary/text_formatting.css");
+            if(key === "VIDEOS_PER_ROW"){
+                setProperty("return-youtube-ui-videos-per-row", value);
+                setInjectionStateHelper( "injection_parts/primary/homepage_videos_per_row.css");
+            }else if(key === "PERCENT_MORE_SPACE_TO_ACTIONS_BAR"){
+                setProperty("return-youtube-ui-percent-more-actions-bar-space", value+"%");
+                setInjectionStateHelper( "injection_parts/primary/percent_more_actions_bar.css");
             }
         }
     }).catch((error) => {
@@ -229,8 +209,8 @@ settingsToActions();
 
 // SPECIFICS TO YOUTUBE.COM
 /*
-* SPECIFIC TO YOUTUBE - listens for all video changes and re-applies
-* */
+SPECIFIC TO YOUTUBE - listens for all video changes and re-applies
+*/
 const videoObserver = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
         if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
